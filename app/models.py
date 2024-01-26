7e71e6ddd24c3af747679a9fc904edf67f638290
Martin Crabtree <martin.crabtree@gmail.com>
2024-01-26 16:01:30 -0500

    creating the relationships

--- app/models.py
+++ app/models.py
@@ -4,9 +4,10 @@ from .extensions import db
 class Course(db.Model):
     id = db.Column(db.Integer, primary_key=True)    #every model needs an id
     name = db.Column(db.String(50), unique=True)    #string with 50 char limit
-
+    students = db.relationship("Student", back_populates="course")
 
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)  #every model needs an id  
     name = db.Column(db.String(50), unique=True)    #string with 50 char limit
     course_id = db.Column(db.ForeignKey("course.id"))
+    course = db.relationship("Course", back_populates="students")
