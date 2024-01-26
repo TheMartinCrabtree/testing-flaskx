e6a92a3db4bbe34b94c7184649533dec41c378b7
Martin Crabtree <martin.crabtree@gmail.com>
2024-01-26 12:12:50 -0500

    testing sqlite updates

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,12 @@
 from flask import Flask
+from .extensions import api, db
 
 def create_app():
     app = Flask(__name__)
+    
+    app.config["SQLALCHEMY_DATABASE_URI"]
+
+    api.init_app(app)
+    db.init_app(app)
+
     return app
