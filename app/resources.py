22f6f486c048a08326254d48a1a6803d37643fcf
Martin Crabtree <martin.crabtree@gmail.com>
2024-01-30 12:42:07 -0500

    testing out endpoints

--- app/resources.py
+++ app/resources.py
@@ -1,5 +1,8 @@
 from flask_restx import Resource, Namespace
 
+from .api_models import course_model
+from .models import Course
+
 ns = Namespace("api")
 
 
@@ -7,3 +10,10 @@ ns = Namespace("api")
 class Hello(Resource):
     def get(self):
         return {"hello": "restx"}
+    
+@ns.route("/courses")
+class CourseAPI(Resource):
+    # coerce course model into json friendly data
+    @ns.marshal_list_with(course_model)
+    def get(self):
+        return Course.query.all()
