6b7992fd7f9f01605739bc53b0959104eab319c5
Martin Crabtree <martin.crabtree@gmail.com>
2024-02-09 14:28:18 -0500

    nesting models to set up relational db

--- app/api_models.py
+++ app/api_models.py
@@ -2,13 +2,14 @@ from flask_restx import fields
 
 from .extensions import api
 
-course_model = api.model("Course", {
-    "id": fields.Integer,
-    "name": fields.String
-    #"students":
-})
-
 student_model = api.model("Student", {
     "id": fields.Integer,
-    "name": fields.String
+    "name": fields.String,
+    "course": fields.Nested(course_model)
+})
+
+course_model = api.model("Course", {
+    "id": fields.Integer,
+    "name": fields.String,
+    "students": fields.List(fields.Nested(student_model))
 })
